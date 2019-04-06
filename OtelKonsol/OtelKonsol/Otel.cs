1bd8f81243eb2299ad2f8f3e38d3ebb7a98018c3
damlaipci <damla.ipci@ogr.deu.edu.tr>
2019-04-06 21:20:26 +0300

    00

--- OtelKonsol/OtelKonsol/Otel.cs
+++ OtelKonsol/OtelKonsol/Otel.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace OtelKonsol
 {
-    class Program
+    class Otel
     {
-        static void Main(string[] args)
-        {
-            
-        }
     }
 }
