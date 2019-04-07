d9856a1a68c2ab92cf5388614457e77acb2708f0
damlaipci <damla.ipci@ogr.deu.edu.tr>
2019-04-07 18:55:37 +0300

    int

--- OtelKonsol/OtelKonsol/Otel.cs
+++ OtelKonsol/OtelKonsol/Otel.cs
@@ -8,6 +8,10 @@ namespace OtelKonsol
 {
     class Otel
     {
-        
+        Oda[] odalar = new Oda[36]; //Oda tipinde odalar dizisi.
+        string[,] takvim = new string[36, 60];//36 oda için 60 günlük takvim oluşturduk
+        int[,] kullanici = new int[36, 60];//kullanıcının seceği
+        DateTime baslangic1 = DateTime.Today;//baslangıc tarihi
+        DateTime bitis1 = DateTime.Today;//bitis tarihi
     }
 }
