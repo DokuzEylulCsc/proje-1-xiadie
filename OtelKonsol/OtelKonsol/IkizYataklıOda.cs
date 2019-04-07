757fb16d8b9df90a3bf5030cd4472b7754511a75
damlaipci <damla.ipci@ogr.deu.edu.tr>
2019-04-07 18:51:16 +0300

    ikiz

--- OtelKonsol/OtelKonsol/IkizYataklıOda.cs
+++ OtelKonsol/OtelKonsol/IkizYataklıOda.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace OtelKonsol
 {
-    class IkizYataklıOda
+    class IkizYataklıOda:Oda
     {
+        public IkizYatakliOda(int no, string manzaraTip)
+        {
+            OdaNumarasi = no + 1;
+            YatakCesidi = "Ikiz";
+            if (manzaraTip == "Havuz")
+                HavuzManzarasi = true;
+            else if (manzaraTip == "Orman")
+                OrmanManzarasi = true;
+            else
+                DenizManzarasi = true;
+
+        }
+        public override void RezervasyonYap(int odaNo, DateTime baslangic, DateTime bitis)
+        {
+
+        }
+        public override void RezervasyonIptal()
+        {
+
+        }
     }
 }
