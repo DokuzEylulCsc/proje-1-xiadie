d13f1a5badf3ecb3f8bc71090ae3275e74776d35
melikeyilmaz <melike.yilmaz@ogr.deu.edu.tr>
2019-04-07 19:06:57 +0300

    otelz

--- OtelKonsol/OtelKonsol/Rezervasyon.cs
+++ OtelKonsol/OtelKonsol/Rezervasyon.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace OtelKonsol
 {
-    class Rezervasyon
+    class Rezervasyon:Otel
     {
-    }
+        internal void RezervasyonYap(int isim, string tip, bool havuz, bool deniz, bool orman, Oda[] odalar, DateTime baslangic, DateTime bitis, string[,] takvim, int[,] kullanici)//dışarıdan girilenler
+        {
+            bool check = false;
+            for (int i = 0; i < 36; i++)
+            {
+                if (havuz == true && odalar[i].HavuzManzarasi == true && tip == odalar[i].YatakCesidi)//dışardan girilen manzara ve oda tiplerine bakıyor
+                {
+                    check = TariheBak(isim, odalar, takvim, baslangic, bitis, i, kullanici);//odanın boşluğuna bakıyor
+                    if (check)
+                    {
+                        Console.WriteLine(odalar[i].OdaNumarasi + " " + odalar[i].YatakCesidi);//onlara uygun odayı veriyo
+                        break;
+                    }
+                }
+                else if (deniz == true && odalar[i].DenizManzarasi == true && tip == odalar[i].YatakCesidi)//dışardan girilen manzara ve oda tiplerine bakıyor
+                {
+                    check = TariheBak(isim, odalar, takvim, baslangic, bitis, i, kullanici);
+                    if (check)
+                    {
+                        Console.WriteLine(odalar[i].OdaNumarasi + " " + odalar[i].YatakCesidi);
+                        break;
+                    }
+                }
+                else if (orman == true && odalar[i].OrmanManzarasi == true && tip == odalar[i].YatakCesidi)//dışardan girilen manzara ve oda tiplerine bakıyor
+                {
+                    check = TariheBak(isim, odalar, takvim, baslangic, bitis, i, kullanici);
+                    if (check)
+                    {
+                        Console.WriteLine(odalar[i].OdaNumarasi + " " + odalar[i].YatakCesidi);
+                        break;
+                    }
+                }
+
+            }
+        }
 }
