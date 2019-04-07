f0e2f9a3d93dc787b56e1a716e65e826cb692c22
damlaipci <damla.ipci@ogr.deu.edu.tr>
2019-04-07 18:40:55 +0300

    aaa

--- OtelKonsol/OtelKonsol/Program.cs
+++ OtelKonsol/OtelKonsol/Program.cs
@@ -10,10 +10,7 @@ namespace OtelKonsol
     {
         static void Main(string[] args)
         {
-            kazlkgal
-
-
-                77alo alo alo alo
+            damladamla
         }
     }
 }
