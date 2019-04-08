50854281dc929369fbb7a19a06e31f5e4ad543ab
melikeyilmaz <melike.yilmaz@ogr.deu.edu.tr>
2019-04-08 19:16:46 +0300

    yenifikir2

--- OtelKonsol/OtelKonsol/Rezervasyon.cs
+++ OtelKonsol/OtelKonsol/Rezervasyon.cs
@@ -6,96 +6,191 @@ using System.Threading.Tasks;
 
 namespace OtelKonsol
 {
-    class Rezervasyon : Otel
+    class Rezervasyon
     {
-        internal void RezervasyonYap(int isim, string tip, bool havuz, bool deniz, bool orman, Oda[] odalar, DateTime baslangic, DateTime bitis, string[,] takvim, int[,] kullanici)//dışarıdan girilenler
+
+        public static bool rezerve(Otel otel, string musteriTC, string odaTipi, string manzarası
+            , DateTime start, DateTime end)
         {
-            bool check = false;
-            for (int i = 0; i < 36; i++)
+
+            double price = 0;
+            int sec = sorgu(otel, musteriTC, odaTipi, manzarası, start, end, true);
+            if (sec < 0) return false;
+            else
             {
-                if (havuz == true && odalar[i].HavuzManzarasi == true && tip == odalar[i].YatakCesidi)//dışardan girilen manzara ve oda tiplerine bakıyor
-                {
-                    check = TariheBak(isim, odalar, takvim, baslangic, bitis, i, kullanici);//odanın boşluğuna bakıyor
-                    if (check)
-                    {
-                        Console.WriteLine(odalar[i].OdaNumarasi + " " + odalar[i].YatakCesidi);//onlara uygun odayı veriyo
-                        break;
-                    }
-                }
-                else if (deniz == true && odalar[i].DenizManzarasi == true && tip == odalar[i].YatakCesidi)//dışardan girilen manzara ve oda tiplerine bakıyor
-                {
-                    check = TariheBak(isim, odalar, takvim, baslangic, bitis, i, kullanici);
-                    if (check)
-                    {
-                        Console.WriteLine(odalar[i].OdaNumarasi + " " + odalar[i].YatakCesidi);
-                        break;
-                    }
-                }
-                else if (orman == true && odalar[i].OrmanManzarasi == true && tip == odalar[i].YatakCesidi)//dışardan girilen manzara ve oda tiplerine bakıyor
-                {
-                    check = TariheBak(isim, odalar, takvim, baslangic, bitis, i, kullanici);
-                    if (check)
-                    {
-                        Console.WriteLine(odalar[i].OdaNumarasi + " " + odalar[i].YatakCesidi);
-                        break;
-                    }
-                }
-
-            }
-        }
-        private bool TariheBak(int isim, Oda[] odalar, string[,] takvim, DateTime baslangic, DateTime bitis, int odaNo, int[,] kullanici)
-        {
-            int k = Convert.ToInt32(bitis.Day) - Convert.ToInt32(baslangic.Day);
-            int k2 = 0;
-            for (int j = baslangic.Day; j < bitis.Day; j++)//baslangıc ve bitis tarihinin düzgün olması
-            {
-                if (takvim[odaNo, j] == " - ")//eger oda boşsa
-                {
-                    k2++;
-                    if (k2 == k)
-                    {
-                        for (int t = baslangic.Day; t < bitis.Day; t++)
-                        {
-                            takvim[odaNo, t] = " D ";//o odayı o tarihlerde doldur.
-
-                            kullanici[odaNo, t] = isim;
-
-                        }
-                        return true;
-                    }
-                }
-            }
-            Console.WriteLine("o tarihlerde kritere uyan oda bulunamadı");
-            return false;
-
-
-        }
-        public void OdaMusaitMi(Oda[] odalar, string[,] takvim)
-        {
-            for (int i = 0; i < 36; i++)
-            {
-                for (int j = 0; j < 60; j++)
-                {
-                    Console.Write(takvim[i, j]);// tahvimi gösterir
-
-                }
-                Console.WriteLine();
+                if (odaTipi.Equals("Tek Yatakli Oda"))
+                    price = otel.tek[sec].rezervasyonYap(start, end, musteriTC);
+                else if (odaTipi.Equals("Cift Yatakli Oda"))
+                    price = otel.cift[sec].rezervasyonYap(start, end, musteriTC);
+                else if (odaTipi.Equals("Ikiz Yatakli Oda"))
+                    price = otel.ikiz[sec].rezervasyonYap(start, end, musteriTC);
+                Console.WriteLine("Toplam ucret = " + price);
+                return true;
             }
 
         }
 
-        public void RezervasyonIptal(Oda[] odalar, int[,] kullanici, string[,] takvim, int isim)
-        {
-            for (int i = 0; i < 36; i++)
-            {
-                for (int j = 0; j < 60; j++)
-                {
-                    if (isim == kullanici[i, j])
-                        takvim[i, j] = " - ";// takvimde D olan kısımları "-" yapar.
 
-                }
+
+        public static bool iptal(Otel otel, string musteriTC, string odaTipi, string manzarası
+           , DateTime start, DateTime end)
+        {
+
+
+            int sec = sorgu(otel, musteriTC, odaTipi, manzarası, start, end, false);
+            if (sec < 0) return false;
+            else
+            {
+                if (odaTipi.Equals("Tek Yatakli Oda"))
+                    otel.tek[sec].rezervasyonIptal(start, end, musteriTC);
+                else if (odaTipi.Equals("Cift Yatakli Oda"))
+                    otel.cift[sec].rezervasyonIptal(start, end, musteriTC);
+                else if (odaTipi.Equals("Ikiz Yatakli Oda"))
+                    otel.ikiz[sec].rezervasyonIptal(start, end, musteriTC);
+                return true;
             }
 
+        }
+
+        // sorgu tipi ekleme ise true döner iptal ise false döner
+        public static int sorgu(Otel otel, string musteriTC, string odaTipi, string manzarası
+         , DateTime start, DateTime end, bool sorguTipi)
+        {
+
+            if (odaTipi.Equals("Tek Yatakli Oda"))
+            {
+                if (manzarası.Equals("Orman"))
+                {
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        if (otel.tek[i].rezervasyonSorgu(start, end)) return i;
+                        else if (!otel.tek[i].rezervasyonSorgu(start, end) && !sorguTipi)
+                            return i;
+
+                    }
+                }
+                else if (manzarası.Equals("Deniz"))
+                {
+                    for (int i = 4; i <= 7; i++)
+                    {
+                        if (otel.tek[i].rezervasyonSorgu(start, end)) return i;
+                        else if (!otel.tek[i].rezervasyonSorgu(start, end) && !sorguTipi)
+                            return i;
+
+
+                    }
+
+                }
+                else if (manzarası.Equals("Havuz"))
+                {
+                    for (int i = 8; i <= 11; i++)
+                    {
+                        if (otel.tek[i].rezervasyonSorgu(start, end)) return i;
+                        else if (!otel.tek[i].rezervasyonSorgu(start, end) && !sorguTipi)
+                            return i;
+
+
+                    }
+
+                }
+
+
+            }
+            else if (odaTipi.Equals("Ikiz Yatakli Oda"))
+            {
+                if (manzarası.Equals("Orman"))
+                {
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        if (otel.ikiz[i].rezervasyonSorgu(start, end)) return i;
+                        else if (!otel.ikiz[i].rezervasyonSorgu(start, end) && !sorguTipi)
+                            return i;
+
+
+                    }
+                }
+                else if (manzarası.Equals("Deniz"))
+                {
+                    for (int i = 4; i <= 7; i++)
+                    {
+                        if (otel.ikiz[i].rezervasyonSorgu(start, end)) return i;
+                        else if (!otel.ikiz[i].rezervasyonSorgu(start, end) && !sorguTipi)
+                            return i;
+
+
+                    }
+
+                }
+                else if (manzarası.Equals("Havuz"))
+                {
+                    for (int i = 8; i <= 11; i++)
+                    {
+                        if (otel.ikiz[i].rezervasyonSorgu(start, end)) return i;
+                        else if (!otel.ikiz[i].rezervasyonSorgu(start, end) && !sorguTipi)
+                            return i;
+
+
+                    }
+
+                }
+
+
+
+            }
+            else if (odaTipi.Equals("Cift Yatakli Oda"))
+            {
+                if (manzarası.Equals("Orman"))
+                {
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        if (otel.cift[i].rezervasyonSorgu(start, end)) return i;
+                        else if (!otel.cift[i].rezervasyonSorgu(start, end) && !sorguTipi)
+                            return i;
+
+
+                    }
+                }
+                else if (manzarası.Equals("Deniz"))
+                {
+                    for (int i = 4; i <= 7; i++)
+                    {
+                        if (otel.cift[i].rezervasyonSorgu(start, end)) return i;
+                        else if (!otel.cift[i].rezervasyonSorgu(start, end) && !sorguTipi)
+                            return i;
+
+
+                    }
+
+                }
+                else if (manzarası.Equals("Havuz"))
+                {
+                    for (int i = 8; i <= 11; i++)
+                    {
+                        if (otel.cift[i].rezervasyonSorgu(start, end)) return i;
+                        else if (!otel.cift[i].rezervasyonSorgu(start, end) && !sorguTipi)
+                            return i;
+
+
+
+                    }
+
+                }
+
+
+            }
+            return -1;
+        }
+
+
+
+
+
+
+        public static void istatistikSorgu(Otel otel)  //İstatiksel Sorgu ile otelimizin doluluk oranına bakıyoruz.
+        {
+            otel.dolulukOranı();
         }
     }
+
+
 }
